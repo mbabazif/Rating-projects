2a767882b67dfd893d8f83178317d804a4de58b7
Florence <mbababziflorence81@gmail.com>
2019-04-01 12:14:31 +0200

    first commit

---
+++ reviewapp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+import datetime as dt
+
+class tags(models.Model):
+    name = model.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
+    def save_tags(self):
+        self.save()
+
+    def delete_tags(self):
+        self.delete()
+
+class Locattion(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.name
+    
+class Project(models.Model):
+    title = models.TextField(max_length=200, null=True, blank=True, default="title")
+    project_image = models.ImageField(upload_to='picture/', null=True ,blank=True)
+    description = models.TextField()
+    project_url=model.URLField(max_length=300)
+
