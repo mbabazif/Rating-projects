f18f020a60172f67049fdbf0ddfe071f730aa80b
Florence <mbababziflorence81@gmail.com>
2019-04-01 13:18:26 +0200

    adding the class project and the instances in the models

--- reviewapp/models.py
+++ reviewapp/models.py
@@ -39,5 +39,18 @@ class Profile (models.Model):
     
     bio = models.TextField(max_length=100, null=True, blank=True, default="bio")
     profile_pic = models.ImageField(upload_to='picture/', null=True, blank=True, default=0)
-    user = 
+    user = models.OneToOneField(User, oon_delete=model.CASCADE, null=True, related_name="profile")
+    project=models.ForeignKey(Project,nulel=True)
+    contact= models.IntegerField(default=0)
+
+class Image(models.Model):
+    image=models.ImageField(upload_to='picture/', )
+    name = models.CharField(max_length=30)
+    user= models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="images")
+    description=models.TextField()
+    location=models.ForeignKey(Location , null=True)
+    tags= model.ManyToManyField(tags,blank=True)
+    # likes = models.IntegerField(default=0)
+    # comments= models.TextField(blank=True)
+
 
